583493e48f456a0bd871dfca2fc5bc34e3ec2d17
Igor <isidoroigor9@gmail.com>
2024-05-17 23:18:28 -0300

    Configuração inicial

---
+++ src/fetch_products.py
@@ -0,0 +1,96 @@
+import requests
+import json
+from urllib.parse import quote
+
+# Base URL for the Mercadolivre search API
+REQUEST_URL = 'https://api.mercadolibre.com/sites/MLB/search'
+
+def fetch_products_in_category(query, category_id):
+  """
+  Fetches products from the MercadoLivre API based on a search query and category ID.
+  
+  Parameters:
+    query (str): The search term.
+    category_id (str): The category ID.
+      
+  Returns:
+    list: A list of dictionaries with product details.
+  """
+
+  # Encode the query to be URL-safe
+  query_encoded = quote(query)
+  products = []
+  offset = 0
+
+  try:
+    # Initial API request
+    response = requests.get(f'{REQUEST_URL}?q={query_encoded}&category={category_id}').json()
+
+    # Continue fetching products while offset is within the total number of products
+    while (offset <= (response['paging']['total'] - 50)) and offset <= 1000:
+      offset += 50
+
+      # Request the next batch of products with the updated offset
+      response = requests.get(f'{REQUEST_URL}?q={query_encoded}&category={category_id}&offset={offset}').json()
+
+      # Extract relevant product details and add them to the products list
+      for product in response['results']:
+        products.append({
+          'id': product['id'],
+          'title': product['title'],
+          'permalink': product['permalink'],
+          'price': product['price'],
+        })
+
+  except requests.RequestException as e:
+    # Handle any request-related exceptions
+    print(f'An error occurred while fetching products: {e}')
+  except Exception as e:
+    # Handle any other unexpected exceptions
+    print(f'An unexpected error occurred: {e}')
+
+  return products
+
+def fetch_products_by_categories(query, categories_list):
+  """
+  Fetches products for a query across multiple categories from the MercadoLivre API.
+  
+  Parameters:
+    query (str): The search term.
+    categories_list (list): A list of category dictionaries.
+      
+  Returns:
+    dict: A dictionary with category names as keys and lists of products as values.
+  """
+   
+  products_by_categories = {}
+  for category in categories_list:
+    # Fetch products for each category and store them in the dictionary
+    products_by_categories[category['name']] = fetch_products_in_category(query, category['id'])
+
+  print(query, '(OK)')
+  return products_by_categories
+
+def fetch_products_for_plants(categories_list, plant_list):
+  """
+  Fetches products for a list of plants (by scientific and popular names) from the MercadoLibre API
+  and saves the results to JSON files.
+  
+  Parameters:
+    categories_list (list): A list of category dictionaries.
+    plant_list (list): A list of tuples with plant scientific and popular names.
+      
+  Returns:
+    None
+  """
+
+  for plant in plant_list:
+    # Fetch products by scientific and popular names for each plant
+    products = {
+      'by_scientific_name': fetch_products_by_categories(query=plant[0], categories_list=categories_list),
+      'by_popular_name': fetch_products_by_categories(query=plant[1], categories_list=categories_list),
+    }
+
+    # Save the fetched products to a JSON file named after the plant's popular name
+    with open(f'../outputs/{plant[1]}.json', 'w') as json_file:
+      json_file.write(json.dumps(products, indent=2))
---
+++ src/main.py
@@ -0,0 +1,50 @@
+import json
+import threading
+from fetch_products import fetch_products_for_plants
+from datetime import datetime
+
+N_THREADS = 3
+
+# Get the start time for measuring execution time
+start_time = datetime.now()
+
+# Load categories from JSON file
+with open('../data/categories.json') as categories_file:
+  categories = (json.load(categories_file))['categories']
+
+# Load plants from JSON file
+with open('../data/plants.json') as plants_file:
+  plants = list((json.load(plants_file).items()))
+
+# Separate the plants list into N_THREADS slices
+slice_size = round(len(plants) / N_THREADS)
+plant_slices = []
+pointer = 0
+for index in range(N_THREADS):
+  if (pointer + slice_size >= len(plants) or index + 1 == N_THREADS):
+    slice = plants[pointer:]
+    plant_slices.append(slice)
+  else:
+    slice = plants[pointer: pointer + slice_size]
+    plant_slices.append(slice)
+  pointer += slice_size
+
+# Create threads to fetch products for each slice of plants
+threads = []
+for i in range(N_THREADS):
+  thread = threading.Thread(target=fetch_products_for_plants, args=(categories, plant_slices[i]))
+  threads.append(thread)
+  thread.start()
+
+# Wait for all threads to complete
+for thread in threads:
+  thread.join()
+
+# Display execution summary
+print('\n\n================================================================')
+finish_time = datetime.datetime.now()
+start_time_string = start_time.strftime('%d/%m/%Y %H:%M')
+finish_time_string = finish_time.strftime('%d/%m/%Y %H:%M')
+print('The execution started at ' + start_time_string)
+print('The execution finished at ' + finish_time_string)
+print('================================================================\n')
