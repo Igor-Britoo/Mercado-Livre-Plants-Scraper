52bb41bb7332b7ef1a2de7bdd7856d14f4561789
Igor <isidoroigor9@gmail.com>
2024-06-30 23:47:08 -0300

    Implementação de comutação entre os tipos de extração de dados

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
 import json
 import threading
-from fetch_products import fetch_and_save_products_for_plants
 from datetime import datetime
 
-N_THREADS = 5
+N_THREADS = 3   # Number of threads
+IS_FETCHING = True  # If True its going to fetch the data from the API.
+                    # If False its going to scrape the data from the marketplace website.
+
+# Choose the appropriate import based on the IS_FETCHING flag
+if IS_FETCHING:
+  from fetch_products import fetch_and_save_products_for_plants
+else:
+  from scrape_products import scrape_and_save_products_for_plants
 
 # Get the start time for measuring execution time
 start_time = datetime.now()
 
-# Load categories from JSON file
-with open('../data/categories.json') as categories_file:
-  categories = (json.load(categories_file))['categories']
+# Load categories from JSON file if IS_FETCHING is True
+if IS_FETCHING:
+  with open('../data/categories.json') as categories_file:
+    categories = (json.load(categories_file))['categories']
 
 # Load plants from JSON file
 with open('../data/plants.json') as plants_file:
@@ -29,10 +37,13 @@ for index in range(N_THREADS):
     plant_slices.append(slice)
   pointer += slice_size
 
-# Create threads to fetch products for each slice of plants
+# Create threads to fetch or scrape products for each slice of plants
 threads = []
 for i in range(N_THREADS):
-  thread = threading.Thread(target=fetch_and_save_products_for_plants, args=(categories, plant_slices[i]))
+  if IS_FETCHING:
+    thread = threading.Thread(target=fetch_and_save_products_for_plants, args=(categories, plant_slices[i]))
+  else:
+    thread = threading.Thread(target=scrape_and_save_products_for_plants, args=(plant_slices[i], ))
   threads.append(thread)
   thread.start()
 
